0d513e10042a94c824cb959d2de1edcc040cb57d
Daniel Bevenius <daniel.bevenius@gmail.com>
2022-09-21 13:21:37 +0200

    Add struct example

---
+++ src/struct.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+type something struct {
+	name string
+	x int
+}
+
+func main() {
+	fmt.Printf("Struct example\n");
+	var _ something 
+	s := something {}
+	fmt.Printf("s.name: %s, s.x: %d\n", s.name, s.x)
+
+	s2 := something { "Fletch", 47}
+	fmt.Printf("s2.name: %s, s2.x: %d\n", s2.name, s2.x)
+
+	anon := struct {
+		name string
+		x int
+	} {
+		name: "Fletch",
+		x: 47,
+	}
+	fmt.Printf("anon.name: %s, anon.x: %d\n", anon.name, anon.x)
+}
